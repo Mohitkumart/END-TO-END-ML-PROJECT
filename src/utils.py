868f2175187593ef8754e27d354b9263816508da
Mohit Kumar <mohitk0258@gmail.com>
2025-06-05 15:51:07 +0530

    prediction pipeline, templets and app.py

--- src/utils.py
+++ src/utils.py
@@ -81,3 +81,11 @@ def evaluate_models(X_train,y_train, X_test,y_test,models, params):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,mode='rb') as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
